b103b2da65c5d82cec32aea443020f0e17af0760
Dmitry Sinyagin <itk_system@mail.ru>
2024-11-24 13:44:22 +0300

    send payment completed into rabbit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,23 @@ const warehouseClient   = new WarehouseServiceClientHandler();
 const CommonFunctionHelper = require("openfsm-common-functions")
 const commonFunction= new CommonFunctionHelper();
 const authMiddleware = require('openfsm-middlewares-auth-service'); // middleware для проверки токена
+const ClientProducerAMQP  =  require('openfsm-client-producer-amqp'); // ходим в почту через шину
 const { v4: uuidv4 } = require('uuid'); 
+
+/* Коннектор для шины RabbitMQ */
+const { RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASSWORD, RABBITMQ_COMPLETED_ACTION_QUEUE, RABBITMQ_FAILED_ACTION_QUEUE } = process.env;
+/*
+const amqp = require('amqplib');
+
+const login = RABBITMQ_USER || 'guest';
+const pwd = RABBITMQ_PASSWORD || 'guest';
+const completed_queue = RABBITMQ_COMPLETED_ACTION_QUEUE || 'PAYMENT_COMPLETED_ACTION';
+const failed_queue = RABBITMQ_FAILED_ACTION_QUEUE || 'PAYMENT_FAILED_ACTION';
+const host = RABBITMQ_HOST || 'localhost';
+const port = RABBITMQ_PORT || '5672';
+*/
+
+
 require('dotenv').config();
 
 
@@ -31,30 +47,49 @@ exports.create = async (req, res) => {
     let userId = await authMiddleware.getUserId(req, res);
     if (!userId ) return sendResponse(res, 400, { message: "Invalid user ID" });         
     try {
+// проверили количество в корзине         
         const basketCount =
              await warehouseClient.getBasket(commonFunction.getJwtToken(req));
         if(!basketCount 
             || basketCount.data.basket.length == 0) 
                 throw(404)
+// проверили доступность товаров                
         const productAvailability =     
             await warehouseClient.productAvailability(commonFunction.getJwtToken(req)); // проверка на доступность товара
-        if(!productAvailability || !productAvailability?.availabilityStatus)
+        if(!productAvailability || !productAvailability?.data?.availabilityStatus)
             throw(409)
 
-       /* Резервируем товар */
+// Создаем заказ
         let order = new OrderDto(await orderHelper.create(userId, referenceId ));
         if (!order) 
              throw(common.HTTP_CODES.SERVICE_UNAVAILABLE)
-        const warehouseClientResponse = await warehouseClient.createOrder( commonFunction.getJwtToken(req),  { orderId : order.getOrderId()});
+
+// привязали товары в корзине к заказу        
+        const warehouseClientResponse = 
+            await warehouseClient.createOrder( commonFunction.getJwtToken(req),  { orderId : order.getOrderId()});
         if(!warehouseClientResponse.success) 
              throw(warehouseClientResponse.status)
+
+// посчитали сумму заказа            
         order.setTotalAmount(warehouseClientResponse.data.totalAmount);
+
+// Отправили заказ на обработку службами
+        await exports.processMessage( process.env.RABBITMQ_COMPLETED_ACTION_QUEUE, 'PAYMENT_COMPLETED', order )        
+
+// Ответили фронту об успехе        
         sendResponse(res, 200, { status: true,  order });
     } catch (error) {
          console.error("Error create:", error);
+// Отправили ОТМЕНУ заказа на обработку службами        
+         try {
+            await exports.processMessage( process.env.RABBITMQ_FAILED_ACTION_QUEUE, 'PAYMENT_FAILED', order )         
+              } catch (error) {            
+        }
+// Ответили фронту о НЕУДАЧЕ        
          sendResponse(res, (Number(error) || 500), { code: (Number(error) || 500), message:  new CommonFunctionHelper().getDescriptionByCode((Number(error) || 500)) });
     }
 };
+
 
 exports.decline = async (req, res) => {    
     let userId = await authMiddleware.getUserId(req, res);
@@ -102,3 +137,13 @@ exports.getOrder = async (req, res) => {
     }
 };
 
+  // Основная функция для обработки сообщения из очереди
+  exports.processMessage = async (queue, process, msg) => {    
+    try {
+         const rabbitClient = new ClientProducerAMQP( process,  process.env.RABBITMQ_USER,   process.env.RABBITMQ_PASSWORD  );
+         await  rabbitClient.sendMessage(queue, {process, msg })  
+      } catch (error) {            
+        throw(error)
+    }
+  }
+  
