22e77bb15e3518929c3c0c2c41f5ef688371df22
Dmitry Sinyagin <itk_system@mail.ru>
2024-12-19 22:01:11 +0300

    19.12.2024 22:01

--- helpers/OrderHelper.js
+++ helpers/OrderHelper.js
@@ -9,12 +9,13 @@ const amqp = require('amqplib');
 
 /* Коннектор для шины RabbitMQ */
 const { RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASSWORD,  
-  RABBITMQ_ORDER_STATUS_QUEUE, RABBITMQ_WAREHOUSE_DECLINE_QUEUE, RABBITMQ_DELIVERY_DECLINE_QUEUE  } = process.env;
+  RABBITMQ_ORDER_STATUS_QUEUE, RABBITMQ_WAREHOUSE_DECLINE_QUEUE, RABBITMQ_DELIVERY_DECLINE_QUEUE, RABBITMQ_ORDER_DECLINE_QUEUE  } = process.env;
 const login = RABBITMQ_USER || 'guest';
 const pwd = RABBITMQ_PASSWORD || 'guest';
-const ORDER_STATUS_QUEUE = RABBITMQ_ORDER_STATUS_QUEUE || 'ORDER_STATUS';
-const WAREHOUSE_DECLINE_QUEUE = RABBITMQ_WAREHOUSE_DECLINE_QUEUE || 'WAREHOUSE_DECLINE';
-const DELIVERY_DECLINE_QUEUE = RABBITMQ_DELIVERY_DECLINE_QUEUE || 'DELIVERY_DECLINE';
+const ORDER_STATUS_QUEUE       = RABBITMQ_ORDER_STATUS_QUEUE || 'ORDER_STATUS';
+const ORDER_DECLINE_QUEUE      = RABBITMQ_ORDER_DECLINE_QUEUE || 'ORDER_DECLINE';
+const WAREHOUSE_DECLINE_QUEUE  = RABBITMQ_WAREHOUSE_DECLINE_QUEUE || 'WAREHOUSE_DECLINE';
+const DELIVERY_DECLINE_QUEUE   = RABBITMQ_DELIVERY_DECLINE_QUEUE || 'DELIVERY_DECLINE';
 const host = RABBITMQ_HOST || 'rabbitmq-service';
 const port = RABBITMQ_PORT || '5672';
 
@@ -81,7 +82,7 @@ exports.getOrders = (userId) => {
 
 exports.getOrder = (orderId, userId) => {
   return new Promise((resolve, reject) => {      
-    db.query(`SELECT order_id, status, created_at, updated_at from orders where 1=1 and order_id=? and user_id=?`, [orderId, userId],
+    db.query(`SELECT order_id, reference_id, status, created_at, updated_at from orders where 1=1 and order_id=? and user_id=?`, [orderId, userId],
       (err, results) => {
         if (err) {
           return reject(err);
@@ -115,30 +116,44 @@ exports.orderStatusMessage = async (statusMessage) => {
      let rabbitClient = new ClientProducerAMQP();
       await  rabbitClient.sendMessage(ORDER_STATUS_QUEUE, statusMessage )  
     } catch (error) {
-      console.log(`Ошибка ${error} пари отправке статуса заказа ...`);
+      console.log(`Ошибка ${error} при отправке статуса заказа ...`);
   } 
   return;
 }
 
-async function WarehouseOrderDeclineMessage(msg){ // отменить биронирование товара на складе
+async function WarehouseOrderDeclineMessage(statusMessage){ // отменить биронирование товара на складе
   try {
      let rabbitClient = new ClientProducerAMQP();
       await  rabbitClient.sendMessage(WAREHOUSE_DECLINE_QUEUE, statusMessage )  
     } catch (error) {
-      console.log(`Ошибка ${error} пари отправке статуса заказа ...`);
+      console.log(`Ошибка ${error} при отправке статуса заказа ...`);
   } 
   return;
 }
 
-async function DeliveryOrderReservationDeclineMessage (msg){ // отменить бронирование доставки
+async function DeliveryOrderReservationDeclineMessage (statusMessage){ // отменить бронирование доставки
   try {
      let rabbitClient = new ClientProducerAMQP();
       await  rabbitClient.sendMessage(DELIVERY_DECLINE_QUEUE, statusMessage )  
     } catch (error) {
-      console.log(`Ошибка ${error} пари отправке статуса заказа ...`);
+      console.log(`Ошибка ${error} при отправке статуса заказа ...`);
   } 
   return;
 }
+
+async function ReturnTransactionExecuteMessage (statusMessage){ // выполнить операцию возврата средств
+/*  try {
+     let rabbitClient = new ClientProducerAMQP();
+      await  rabbitClient.sendMessage(DELIVERY_DECLINE_QUEUE, statusMessage )  
+    } catch (error) {
+      console.log(`Ошибка ${error} при отправке статуса заказа ...`);
+  } 
+*/
+  console.log('ReturnTransactionExecuteMessage пока не реализован!!!');       
+  return;
+}
+
+
 
 
 // Подключение к RabbitMQ и прослушивание очереди
@@ -151,7 +166,8 @@ async function startConsumer(queue){
 
       channel.consume(queue, async (msg) => {
           if (msg !== null) {
-              await startOrderStatusProducer(msg);
+              console.log(JSON.parse(msg.content.toString()));
+              await startOrderStatusProducer(JSON.parse(msg.content.toString()));
               channel.ack(msg); // Подтверждение обработки сообщения
           }
       });
@@ -160,21 +176,76 @@ async function startConsumer(queue){
   }
 }
 
+async function setOrderStatus(status, orderId){
+  console.log(`Установка статуса ${status} заказу ${orderId}`);
+  return new Promise((resolve, reject) => {      
+    db.query(`update orders
+       set 
+        status = ?
+       where 1=1
+        and order_id=? 
+        `, [status, orderId],
+      (err) => {
+        if (err) {
+          return reject(err);
+        } else
+        resolve(true);                
+      }
+    );
+  });
+};
+
+
 async function startOrderStatusProducer(msg){
   try {        
-      console.log(`Поступило сообщение ${msg}`);
-      await setOrderStatus(msg?.proccesStatus);    //установить статус 
-      if(msg?.status === false){       // откат транзакции
-         await WarehouseOrderDeclineMessage(msg);
-         await DeliveryOrderReservationDeclineMessage(msg);
-      }
+      console.log(`Поступило сообщение ${JSON.stringify(msg)}`);
+      if(!msg) return;
+       await setOrderStatus(msg.processStatus, msg.order.orderId);    //установить статус       
+    } catch (error) {
+      console.error(`Ошибка : ${error}`);
+  }
+}
+
+// Подключение к RabbitMQ и прослушивание очереди ORDER_ROLLBACK_QUEUE
+async function startRollbackConsumer(queue){
+  try {        
+      const connection = await amqp.connect(`amqp://${login}:${pwd}@${host}:${port}`);
+      const channel = await connection.createChannel();
+      await channel.assertQueue(queue, { durable: true });
+      console.log(`Ожидание сообщений в очереди ${queue}...`);
+      channel.consume(queue, async (msg) => {
+          if (msg !== null) {
+              _msg = JSON.parse(msg.content.toString());
+              console.log(_msg);
+              _msg.processStatus = 'DECLINE';
+              await startOrderStatusProducer(_msg); // установить статус ОТКЛОНЕН
+              await orderRollbackProducer(_msg);    // произвести откат операций
+              channel.ack(msg); // Подтверждение обработки сообщения
+          }
+      });
     } catch (error) {
       console.error(`Ошибка подключения к RabbitMQ: ${error}`);
   }
 }
 
 
+// Продьюсер отката операций 
+async function orderRollbackProducer(msg){
+  try {        
+      console.log(`Поступило сообщение ${JSON.stringify(msg)}`);
+      if(!msg) return;      
+      if(msg?.status === false){       // откат транзакции
+          await WarehouseOrderDeclineMessage(msg);
+          await DeliveryOrderReservationDeclineMessage(msg);
+          await ReturnTransactionExecuteMessage(msg);
+       }
+      } catch (error) {
+      console.error(`Ошибка : ${error}`);
+  }
+}
 
-/* Запуск консьюмера ORDER_STATUS_QUEUE*/
-startConsumer(ORDER_STATUS_QUEUE);
+
+
+startConsumer(ORDER_STATUS_QUEUE); /* Запуск консьюмера ORDER_STATUS_QUEUE*/
+startRollbackConsumer(ORDER_DECLINE_QUEUE); /* Запуск консьюмера ORDER_DECLINE_QUEUE*/
 
